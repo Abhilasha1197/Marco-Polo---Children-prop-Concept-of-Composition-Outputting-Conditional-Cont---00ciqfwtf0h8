ee919409bd9c1dc4fd9bbbb28e3be427ef8bff77
abhilasha <abhilasha15597@gmail.com>
2022-11-28 11:11:48 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  
+  let arr = ["Marco" , "Polo"];
+  const [name,setname] = useState(arr[0]);
+  const [btn,setbtn] = useState(arr[1]);
+  const change = () =>{
+    let temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = temp;
+
+    setname(arr[0]);
+    setbtn(arr[1]);
+  }
   return (
     <div id="main">
-
+       <h1 id='marco-polo' >{name}</h1>
+       <button id='marco-polo-toggler' onClick={change}>{btn}</button>
     </div>
   )
 }
